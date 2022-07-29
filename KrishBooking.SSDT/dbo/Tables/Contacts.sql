02f732ca50db43386750649636f20973f9fd447f
PaulLKodjo <maetzp@gmail.com>
2022-07-29 12:07:16 +0000

    Latest

--- KrishBooking.SSDT/dbo/Tables/Contacts.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Contacts] (
-    [ID]              VARCHAR (255) NOT NULL,
-    [UserID]          VARCHAR (255) NULL,
-    [ReasonOfContact] VARCHAR (255) NULL,
-    [Message]         VARCHAR (255) NULL,
-    PRIMARY KEY CLUSTERED ([ID] ASC), 
-    CONSTRAINT [FK_Contacts_To_Users] FOREIGN KEY ([UserID]) REFERENCES [USers]([ID])
-);
-
